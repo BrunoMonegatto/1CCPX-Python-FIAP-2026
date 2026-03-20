2df30985fa3a3694a25f0dea4f890c8d3ead7ad7
brunomonegatto <bmmonegatto@gmail.com>
2026-03-19 22:28:49 -0300

    aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,24 @@
+print("Olá mundo")
+
+print(7+4)
+print("7+4")
+print("7" + "4") #CONCATENAÇÃO DE STRINGS
+#Comentarios de 1 linha
+'''
+    Comentarios de
+    multiplas linhas
+
+'''
+
+# VARIAVEIS
+nome = "Bruno" #str
+idade = 26 # Int
+peso = 65.7 #float
+print(f"{nome} de {idade} anos, você pesa {peso} Kilos.")
+
+#INPUT - SIMULAÇÃO DE FORMS NO CMD
+nome = input("Digite o nome: ")
+idade = int(input("Digite sua idade: "))
+
+print(nome, idade)
+print(idade + 1)
