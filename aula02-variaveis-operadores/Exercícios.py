50ac6b47efa42dc553cafd1506e27f0d171ed1cb
brunomonegatto <bmmonegatto@gmail.com>
2026-03-19 22:59:22 -0300

    aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/Exercícios.py
@@ -0,0 +1,14 @@
+
+nome = input("Digite o nome: ")
+idade = int(input("Digite sua idade: "))
+nasceu = input("Digite sua data de nascimento no formato DD.MM.AAAA: ")
+
+print(f" Bem vindo {nome} de {idade} anos que nasceu em {nasceu}.")
+
+
+conta1 = float(input(f"Digite o primeiro número para a soma : "))
+conta2 = float(input(f"Digite o segundo número para a soma : "))
+resultado = conta1 + conta2
+
+print(f"{nome} de {idade} anos, que nasceu em {nasceu}, o resultado da sua conta com os números {conta1} e {conta2} é {resultado}")
+
